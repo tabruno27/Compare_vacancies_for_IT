16670ae1d7fbfee1d8bcb8fe93e0850dd0b8eab4
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-05-16 11:23:12 +0200

    Update main.py
    Сокращены количество сетевых запросов
    Высушен код
    Обновлена функция def predict_rub_salary
    Избавление от сравнений с None
    Избавление от сравнений с нулём

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import os
 
 
 def print_salary_statistics(statistics, title):
-    vacancy_statistics_table = [['Язык программирования', 'Найдено вакансий', 'Обработано вакансий', 'Средняя зарплата']]
-    for language, salary_statistics in statistics.items():
-        vacancy_statistics_table.append([
+    table_data = [['Язык программирования', 'Найдено вакансий', 'Обработано вакансий', 'Средняя зарплата']]
+    for language, salary_data in statistics.items():
+        table_data.append([
             language,
-            salary_statistics['vacancies_found'],
-            salary_statistics['vacancies_processed'],
-            salary_statistics['average_salary']
+            salary_data['vacancies_found'],
+            salary_data['vacancies_processed'],
+            salary_data['average_salary']
         ])
-    table = AsciiTable(vacancy_statistics_table, title)
+    table = AsciiTable(table_data, title)
     print(table.table)
 
 
@@ -37,8 +37,9 @@ def get_superjob_statistics(programming_languages, headers_superjob, superjob_pa
             print(f"Загрузка вакансий для языка: {language}, страница: {page}")
             response = requests.get(url, headers=headers_superjob, params=params, verify=False)
 
-            if response.ok:
-                pass
+            if not response.ok:
+                print(f"Ошибка при запросе: {response.status_code} - {response.text}")
+                break
 
             superjob_response = response.json()
 
@@ -49,13 +50,11 @@ def get_superjob_statistics(programming_languages, headers_superjob, superjob_pa
                 total_vacancies_processed += 1
                 total_vacancies_found = superjob_response['total']
 
-                salary_from = vacancy.get('payment_from', 0)
-                salary_to = vacancy.get('payment_to', 0)
-                average_salary = calculate_average_salary(salary_from, salary_to)
-
-                total_salary += average_salary
+                expected_salary = predict_rub_salary_sj(vacancy)  
+                total_salary += expected_salary if expected_salary else 0
 
             page += 1
+            time.sleep(1)
 
         if total_vacancies_processed:
             average_salary = int(total_salary / total_vacancies_processed)
@@ -71,67 +70,63 @@ def get_superjob_statistics(programming_languages, headers_superjob, superjob_pa
     return salary_statistics
 
 
-def predict_rub_salary_hh(vacancy):
-    salary = vacancy.get('salary')
-    if not salary or salary['currency'] != 'RUR':
-        return None
-
-    from_salary = salary.get('from')
-    to_salary = salary.get('to')
-
-    return calculate_average_salary(from_salary, to_salary) * 1.2 if from_salary else \
-           calculate_average_salary(from_salary, to_salary) * 0.8 if to_salary else None
-
-
-def get_hh_statistics(programming_languages, area_moscow,vacancies_per_page,headers_hh):
+def get_hh_statistics(programming_languages, area_moscow, vacancies_per_page, headers_hh):
     salary_statistics = {}
     for language in programming_languages:
-        url = f"https://api.hh.ru/vacancies"
+        url = "https://api.hh.ru/vacancies"
         params = {
-            'area': area_moscow,
+            'area': area_moscow,  
             'text': language,
-            'per_page': vacancies_per_page,
-            'page': 0 
+            'per_page': vacancies_per_page,  
+            'page': 0  
         }
-
-        vacancies_found = 0
-        vacancies_processed = 0
-        total_salary = 0
 
         initial_response = requests.get(url, params=params, headers=headers_hh, verify=False)
         initial_response.raise_for_status()
         initial_data = initial_response.json()
-        vacancies_found = initial_data['found']
 
-        pages_number = (vacancies_found // 20) + (1 if vacancies_found % 20 > 0 else 0)
+        vacancies_found = initial_data['found']
+        vacancies = initial_data['items']
+        pages_number = initial_data['pages']  
 
         print(f"Найдено вакансий: {vacancies_found}, страниц: {pages_number}")
 
-        page = 0
-        while page < pages_number:
+        vacancies_processed = 0
+        total_salary = 0
+
+        
+        for vacancy in vacancies:
+            expected_salary = predict_rub_salary_hh(vacancy)
+            if expected_salary is not None:
+                total_salary += expected_salary
+                vacancies_processed += 1
+
+        
+        for page in range(1, pages_number):
             print(f"Загрузка {language}, страница {page + 1} из {pages_number}")
 
+          
             params['page'] = page
             page_response = requests.get(url, params=params, headers=headers_hh, verify=False)
 
+            
             try:
                 page_response.raise_for_status()
             except requests.exceptions.HTTPError as e:
                 print(f"Ошибка при загрузке страницы {page + 1}: {e}")
-                break
+                break  
 
             page_payload = page_response.json()
 
             for vacancy in page_payload['items']:
                 expected_salary = predict_rub_salary_hh(vacancy)
-                if expected_salary is not None:
+                if expected_salary:
                     total_salary += expected_salary
                     vacancies_processed += 1
 
-            page += 1
-            time.sleep(1)
+            time.sleep(1)  
 
-        if vacancies_processed > 0:
+        if vacancies_processed:
             average_salary = int(total_salary / vacancies_processed)
         else:
             average_salary = 0
@@ -143,42 +138,72 @@ def get_hh_statistics(programming_languages, area_moscow,vacancies_per_page,head
         }
 
     return salary_statistics
-    
 
-def calculate_average_salary(salary_from, salary_to):
-    if salary_from and salary_to:
+
+def predict_rub_salary_hh(vacancy):
+    salary = vacancy.get('salary')
+    if not salary or salary['currency'] != 'RUR':
+        return None
+
+    from_salary = salary.get('from')
+    to_salary = salary.get('to')
+
+    return predict_rub_salary({'salary': {'from': from_salary, 'to': to_salary, 'currency': 'RUR'}})
+
+
+def predict_rub_salary_sj(vacancy):
+    salary_from = vacancy.get('payment_from', 0)
+    salary_to = vacancy.get('payment_to', 0)
+
+    return predict_rub_salary({'salary': {'from': salary_from, 'to': salary_to, 'currency': 'RUR'}})
+
+
+def predict_rub_salary(vacancy):
+    
+    if 'currency' in vacancy and vacancy['currency'] != 'RUR':
+        return None
+
+    salary_from = vacancy.get('salary', {}).get('from')
+    salary_to = vacancy.get('salary', {}).get('to')
+
+    if salary_from is not None and salary_to is not None:
         return (salary_from + salary_to) / 2
-    elif salary_from:
-        return salary_from
-    elif salary_to:
-        return salary_to
-    return 0
+    elif salary_from is not None:
+        return salary_from * 1.2
+    elif salary_to is not None:
+        return salary_to * 0.8
+
+    return None
 
 
 def main():
 
     load_dotenv()
     superjob_api_key = os.getenv('SUPERJOB_API_KEY')
-    area_moscow = 1 
-    vacancies_per_page = 20
+    area_moscow = 1  
+    vacancies_per_page = 20  
     superjob_page_count = 100
-    
+
     programming_languages = [
         "JavaScript", "Java", "Python", "Ruby",
         "PHP", "C++", "C#", "Go"
     ]
 
+    
     headers_superjob = {
         'X-Api-App-Id': superjob_api_key,
     }
+
     
     headers_hh = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
 
+    
     hh_statistics = get_hh_statistics(programming_languages, area_moscow,vacancies_per_page, headers_hh)
     superjob_statistics = get_superjob_statistics(programming_languages, headers_superjob, superjob_page_count)
 
+    
     print_salary_statistics(hh_statistics, 'HeadHunter Moscow')
     print_salary_statistics(superjob_statistics, 'SuperJob Moscow')
 
